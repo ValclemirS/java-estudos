8032384c1a42828eab4c655253bb8d861ac3b540
valclemir <valclemir88@gmail.com>
2023-10-18 23:34:38 -0300

    update

---
+++ laçosderepetiçao/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) throws Exception {
+        int number=10;
+        for(int i =0;i<=10;i++){//i incrementa até I=10.
+            System.out.println(i);
+        }
+        System.out.println("Fim da contagem");//quando isso se torna verdade ele sai do FOR para a próxima validação.
+    }
+}
